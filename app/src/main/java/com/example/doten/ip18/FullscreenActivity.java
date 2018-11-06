9cb97dffdad18045560e663754f5f04503cdf420
Relax594 <do.tenelsen@gmail.com>
2018-11-06 13:57:42 +0100

    [ADD] Remaining Time is now being updated every second to match later implementations

--- app/src/main/java/com/example/doten/ip18/FullscreenActivity.java
+++ app/src/main/java/com/example/doten/ip18/FullscreenActivity.java
@@ -2,10 +2,14 @@ package com.example.doten.ip18;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 
 public class FullscreenActivity extends AppCompatActivity {
@@ -19,6 +23,21 @@ public class FullscreenActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_fullscreen);
+        SetRemainingTime();
+    }
+
+    public void SetRemainingTime() {
+        new CountDownTimer(300000, 1000) {
+            TextView remainingTimeTextView = (TextView)findViewById(R.id.remainingtime);
+
+            public void onTick(long millisUntilFinished) {
+                remainingTimeTextView.setText(String.valueOf(millisUntilFinished / 60000));
+            }
+
+            public void onFinish() {
+                remainingTimeTextView.setText("Time expired");
+            }
+        }.start();
     }
 
 
